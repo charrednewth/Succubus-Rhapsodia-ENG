58c447ef98a8b818829189cc97c259b20c074fa6
charrednewth <sollipsis@yahoo.fr>
2016-09-06 01:45:51 +0200

    safekeeping w/ v1.10 changes
    altered original def

--- Scripts - manually insert these/004 - Enemies, classes, people.rb
+++ Scripts - manually insert these/004 - Enemies, classes, people.rb
@@ -33,646 +33,239 @@ module RPG
     def UK_name
       case @id
       
-when 16   #スライム
-   return "Slime"
-when 25   #インプ
-   return "Imp"
-when 27   #キャスト
-   return "Caster"
-when 28   #スライム
-   return "Slime"
-when 31   #レッサーサキュバス
-   return "Lesser Succubus"
-when 32   #レッサーサキュバス
-   return "Lesser Succubus"
-when 33   #レッサーサキュバス
-   return "Lesser Succubus"
-when 34   #レッサーサキュバス
-   return "Lesser Succubus"
-when 35   #レッサーサキュバス
-   return "Lesser Succubus"
-when 36   #レッサーサキュバス
-   return "Lesser Succubus"
-when 38   #サキュバス
-   return "Succubus"
-when 39   #サキュバス
-   return "Succubus"
-when 41   #インプ
-   return "Imp"
-when 42   #インプ
-   return "Imp"
-when 43   #インプ
-   return "Imp"
-when 44   #インプ
-   return "Imp"
-when 45   #インプ
-   return "Imp"
-when 47   #キャスト
-   return "Caster"
-when 48   #キャスト
-   return "Caster"
-when 50   #ナイトメア
-   return "Nightmare"
-when 51   #ナイトメア
-   return "Nightmare"
-when 52   #ナイトメア
-   return "Nightmare"
-when 53   #ナイトメア
-   return "Nightmare"
-when 55   #スライム
-   return "Slime"
-when 56   #スライム
-   return "Slime"
-when 58   #プチウィッチ
-   return "Little Witch"
-when 59   #プチウィッチ
-   return "Little Witch"
-when 60   #プチウィッチ
-   return "Little Witch"
-when 62   #ウィッチ
-   return "Witch"
-when 63   #ウィッチ
-   return "Witch"
-when 65   #サキュバス
-   return "Succubus"
-when 66   #デビル
-   return "Devil"
-when 68   #レッサーサキュバス
-   return "Lesser Succubus"
-when 69   #レッサーサキュバス
-   return "Lesser Succubus"
-when 70   #サキュバス
-   return "Succubus"
-when 72   #ファミリア
-   return "Familiar"
-when 73   #プチウィッチ
-   return "Little Witch"
-when 74   #ウィッチ
-   return "Witch"
-when 75   #フルビュア
-   return "Fulbeua"
-when 77   #デビル
-   return "Devil"
-when 78   #ワーウルフ
-   return "Werewolf"
-when 81   #レッサーサキュバス
-   return "Lesser Succubus"
-when 84   #レッサーサキュバス
-   return "Lesser Succubus"
-when 85   #インプ
-   return "Imp"
-when 86   #キャスト
-   return "Caster"
-when 88   #レッサーサキュバス
-   return "Lesser Succubus"
-when 91   #レッサーサキュバス
-   return "Lesser Succubus"
-when 92   #インプ
-   return "Imp"
-when 93   #ナイトメア
-   return "Nightmare"
-when 95   #レッサーサキュバス
-   return "Lesser Succubus"
-when 96   #インプ
-   return "Imp"
-when 99   #レッサーサキュバス
-   return "Lesser Succubus"
-when 100   #インプ
-   return "Imp"
-when 101   #スライム
-   return "Slime"
-when 104   #レッサーサキュバス
-   return "Lesser Succubus"
-when 105   #インプ
-   return "Imp"
-when 106   #ナイトメア
-   return "Nightmare"
-when 107   #ワーウルフ
-   return "Werewolf"
-when 110   #レッサーサキュバス
-   return "Lesser Succubus"
-when 111   #インプ
-   return "Imp"
-when 112   #ナイトメア
-   return "Nightmare"
-when 113   #ワーウルフ
-   return "Werewolf"
-when 115   #ナイトメア
-   return "Nightmare"
-when 116   #リジェオ
-   return "Rejeo"
-when 119   #レッサーサキュバス
-   return "Lesser Succubus"
-when 120   #プリーステス
-   return "Priestess"
-when 121   #キャスト
-   return "Caster"
-when 122   #サキュバス
-   return "Succubus"
-when 123   #プチウィッチ
-   return "Little Witch"
-when 126   #プチウィッチ
-   return "Little Witch"
-when 127   #ウィッチ
-   return "Witch"
-when 128   #レッサーサキュバス
-   return "Lesser Succubus"
-when 129   #サキュバス
-   return "Succubus"
-when 130   #インプ
-   return "Imp"
-when 131   #ファミリア
-   return "Familiar"
-when 132   #ミミック
-   return "Mimic"
-when 134   #プチウィッチ
-   return "Little Witch"
-when 135   #ウィッチ
-   return "Witch"
-when 136   #レッサーサキュバス
-   return "Lesser Succubus"
-when 137   #サキュバス
-   return "Succubus"
-when 139   #サキュバス
-   return "Succubus"
-when 140   #デビル
-   return "Devil"
-when 141   #レッサーサキュバス
-   return "Lesser Succubus"
-when 144   #ゴブリン
-   return "Goblin"
-when 145   #ギャングコマンダー
-   return "Gang Leader"
-when 146   #インプ
-   return "Imp"
-when 147   #デビル
-   return "Devil"
-when 148   #ナイトメア
-   return "Nightmare"
-when 149   #ナイトメア
-   return "Nightmare"
-when 152   #ゴブリン
-   return "Goblin"
-when 153   #ギャングコマンダー
-   return "Gang Leader"
-when 154   #インプ
-   return "Imp"
-when 155   #デビル
-   return "Devil"
-when 156   #ナイトメア
-   return "Nightmare"
-when 158   #インプ
-   return "Imp"
-when 159   #デビル
-   return "Devil"
-when 162   #サキュバス
-   return "Succubus"
-when 163   #レッサーサキュバス
-   return "Lesser Succubus"
-when 164   #アルラウネ
-   return "Alraune"
-when 165   #マタンゴ
-   return "Matango"
-when 166   #スライム
-   return "Slime"
-when 167   #サキュバス
-   return "Succubus"
-when 170   #サキュバス
-   return "Succubus"
-when 171   #レッサーサキュバス
-   return "Lesser Succubus"
-when 172   #アルラウネ
-   return "Alraune"
-when 173   #マタンゴ
-   return "Matango"
-when 174   #スライム
-   return "Slime"
-when 176   #アルラウネ
-   return "Alraune"
-when 177   #ワーキャット
-   return "Werecat"
-when 178   #ネイジュレンジ
-   return "Neijoronge"
-when 181   #プチウィッチ
-   return "Little Witch"
-when 182   #ウィッチ
-   return "Witch"
-when 183   #サキュバス
-   return "Succubus"
-when 184   #レッサーサキュバス
-   return "Lesser Succubus"
-when 185   #カースメイガス
-   return "Cursed Magus"
-when 186   #ファミリア
-   return "Familiar"
-when 187   #スレイヴ
-   return "Slave"
-when 188   #ミミック
-   return "Mimic"
-when 189   #プチウィッチ
-   return "Little Witch"
-when 190   #ウィッチ
-   return "Witch"
-when 193   #プチウィッチ
-   return "Little Witch"
-when 194   #ウィッチ
-   return "Witch"
-when 195   #サキュバス
-   return "Succubus"
-when 196   #レッサーサキュバス
-   return "Lesser Succubus"
-when 197   #カースメイガス
-   return "Cursed Magus"
-when 198   #ファミリア
-   return "Familiar"
-when 199   #スレイヴ
-   return "Slave"
-when 200   #ミミック
-   return "Mimic"
-when 201   #プチウィッチ
-   return "Little Witch"
-when 202   #ウィッチ
-   return "Witch"
-when 204   #プチウィッチ
-   return "Little Witch"
-when 205   #ウィッチ
-   return "Witch"
-when 206   #レッサーサキュバス
-   return "Lesser Succubus"
-when 207   #サキュバス
-   return "Succubus"
-when 208   #ファミリア
-   return "Familiar"
-when 209   #ゴールドスライム
-   return "Gold Slime"
-when 212   #シルフェ
-   return "Syplhe"
-when 215   #インプ
-   return "Imp"
-when 216   #デビル
-   return "Devil"
-when 217   #デーモン
-   return "Demon"
-when 218   #サキュバス
-   return "Succubus"
-when 219   #レッサーサキュバス
-   return "Lesser Succubus"
-when 220   #リリム
-   return "Lilim"
-when 221   #ダークエンジェル
-   return "Dark Angel"
-when 222   #インプ
-   return "Imp"
-when 223   #デビル
-   return "Devil"
-when 226   #インプ
-   return "Imp"
-when 227   #デビル
-   return "Devil"
-when 228   #デーモン
-   return "Demon"
-when 229   #サキュバス
-   return "Succubus"
-when 230   #レッサーサキュバス
-   return "Lesser Succubus"
-when 231   #リリム
-   return "Lilim"
-when 232   #ダークエンジェル
-   return "Dark Angel"
-when 233   #インプ
-   return "Imp"
-when 234   #デビル
-   return "Devil"
-when 236   #デビル
-   return "Devil"
-when 237   #デーモン
-   return "Demon"
-when 238   #ワーウルフ
-   return "Werewolf"
-when 239   #タマモ
-   return "Tamano"
-when 240   #ユーガノット
-   return "Younganoth"
-when 243   #レッサーサキュバス
-   return "Lesser Succubus"
-when 244   #サキュバス
-   return "Succubus"
-when 245   #サキュバスロード
-   return "Succubus Lord"
-when 246   #インプ
-   return "Imp"
-when 247   #デビル
-   return "Devil"
-when 248   #デーモン
-   return "Demon"
-when 249   #ガーゴイル
-   return "Gargoyle"
-when 250   #リリム
-   return "Lilim"
-when 251   #レッサーサキュバス
-   return "Lesser Succubus"
-when 252   #サキュバス
-   return "Succubus"
-when 255   #レッサーサキュバス
-   return "Lesser Succubus"
-when 256   #サキュバス
-   return "Succubus"
-when 257   #サキュバスロード
-   return "Succubus Lord"
-when 258   #インプ
-   return "Imp"
-when 259   #デビル
-   return "Devil"
-when 260   #デーモン
-   return "Demon"
-when 261   #ガーゴイル
-   return "Gargoyle"
-when 262   #リリム
-   return "Lilim"
-when 263   #レッサーサキュバス
-   return "Lesser Succubus"
-when 264   #サキュバス
-   return "Succubus"
-when 265   #ミミック
-   return "Mimic"
-when 267   #レッサーサキュバス
-   return "Lesser Succubus"
-when 268   #サキュバス
-   return "Succubus"
-when 269   #サキュバスロード
-   return "Succubus Lord"
-when 270   #ミミック
-   return "Mimic"
-when 271   #サキュバスロード
-   return "Succubus Lord"
-when 272   #デーモン
-   return "Demon"
-when 273   #フルビュア
-   return "Fulbeua"
-when 274   #ギルゴーン
-   return "Gilgoon"
-when 277   #レッサーサキュバス
-   return "Lesser Succubus"
-when 278   #キャスト
-   return "Caster"
-when 279   #ワーキャット
-   return "Werecat"
-when 282   #キャスト
-   return "Caster"
-when 283   #キャスト
-   return "Caster"
-when 286   #サキュバス
-   return "Succubus"
-when 287   #サキュバス
-   return "Succubus"
-when 288   #デビル
-   return "Devil"
-when 289   #デビル
-   return "Devil"
-when 290   #ウィッチ
-   return "Witch"
-when 291   #ウィッチ
-   return "Witch"
-when 292   #リリム
-   return "Lilim"
-when 297   #サキュバスロード
-   return "Succubus Lord"
-when 298   #デーモン
-   return "Demon"
-when 301   #ラーミル
-   return "Rarmil"
-when 302   #ヴェルミィーナ
-   return "Vermiena"
-when 303   #ヴェルミィーナ
-   return "Vermiena"
+when 16,28,55,56,101,166,174,421   #スライム
+  return "Slime"
+when 25,41,42,43,44,45,85,92,96,100,105,111,130,146,154,158,215,222,226,233,246,258,407,408   #インプ
+  return "Imp"
+when 26,78,107,113,238,425,426   #ワーウルフ
+  return "Werewolf"
+when 27,47,48,86,121,278,282,283,417,450   #キャスト
+  return "Caster"
+when 31,32,33,34,35,36,68,69,81,84,88,91,95,99,104,110,119,128,136,141,163,171,184,196,206,219,230,243,251,255,263,267,277,312,401,402   #レッサーサキュバス
+  return "Lesser Succubus"
+when 38,39,65,70,122,129,137,139,162,167,170,183,195,207,218,229,244,252,256,264,268,286,287,403,404   #サキュバス
+  return "Succubus"
+when 50,51,52,53,93,106,112,115,148,149,156,419,420   #ナイトメア
+  return "Nightmare"
+when 58,59,60,73,123,126,134,181,189,193,201,204,413,414   #プチウィッチ
+  return "Little Witch"
+when 62,63,74,127,135,182,190,194,202,205,290,291,415,416   #ウィッチ
+  return "Witch"
+when 66,77,140,147,155,159,216,223,227,234,236,247,259,288,289,409,410   #デビル
+  return "Devil"
+when 72,131,186,198,208,423,424   #ファミリア
+  return "Familiar"
+when 75,273,444,568   #フルビュア
+  return "Fulbeua"
+when 116,443,567   #リジェオ
+  return "Rejeo"
+when 120,431   #プリーステス
+  return "Priestess"
+when 132,188,200,265,270,438,439   #ミミック
+  return "Mimic"
+when 144,152,429   #ゴブリン
+  return "Goblin"
+when 145,153,430   #ギャングコマンダー
+  return "Gang Leader"
+when 164,172,176,433,434   #アルラウネ
+  return "Alraune"
+when 165,173,311,435   #マタンゴ
+  return "Matango"
+when 177,279,427,428   #ワーキャット
+  return "Werecat"
+when 178,442,566   #ネイジュレンジ
+  return "Neijoronge"
+when 185,197,432   #カースメイガス
+  return "Cursed Magus"
+when 187,199,418   #スレイヴ
+  return "Slave"
+when 209,422   #ゴールドスライム
+  return "Gold Slime"
+when 212,447,571   #シルフェ
+  return "Sylphe"
+when 217,228,237,248,260,272,298,411,412   #デーモン
+  return "Demon"
+when 220,231,250,262,292,441   #リリム
+  return "Lilim"
+when 221,232,436   #ダークエンジェル
+  return "Dark Angel"
+when 239,440   #タマモ
+  return "Tamano"
+when 240,446,570   #ユーガノット
+  return "Younganoth"
+when 245,257,269,271,297,405,406   #サキュバスロード
+  return "Succubus Lord"
+when 249,261,437   #ガーゴイル
+  return "Gargoyle"
+when 274,445,569   #ギルゴーン
+  return "Gilgoon"
+when 301,448,572   #ラーミル
+  return "Rarmil"
+when 302,303,449,573   #ヴェルミィーナ
+  return "Vermiena"
 when 304   #ラーミルキャスト
-   return "Rarmil Cast"
-when 311   #マタンゴ
-   return "Matango"
-when 312   #レッサーサキュバス
-   return "Lesser Succubus"
-when 401   #レッサーサキュバス
-   return "Lesser Succubus"
-when 402   #レッサーサキュバス
-   return "Lesser Succubus"
-when 403   #サキュバス
-   return "Succubus"
-when 404   #サキュバス
-   return "Succubus"
-when 405   #サキュバスロード
-   return "Succubus Lord"
-when 406   #サキュバスロード
-   return "Succubus Lord"
-when 407   #インプ
-   return "Imp"
-when 408   #インプ
-   return "Imp"
-when 409   #デビル
-   return "Devil"
-when 410   #デビル
-   return "Devil"
-when 411   #デーモン
-   return "Demon"
-when 412   #デーモン
-   return "Demon"
-when 413   #プチウィッチ
-   return "Little Witch"
-when 414   #プチウィッチ
-   return "Little Witch"
-when 415   #ウィッチ
-   return "Witch"
-when 416   #ウィッチ
-   return "Witch"
-when 417   #キャスト
-   return "Caster"
-when 418   #スレイヴ
-   return "Slave"
-when 419   #ナイトメア
-   return "Nightmare"
-when 420   #ナイトメア
-   return "Nightmare"
-when 421   #スライム
-   return "Slime"
-when 422   #ゴールドスライム
-   return "Gold Slime"
-when 423   #ファミリア
-   return "Familiar"
-when 424   #ファミリア
-   return "Familiar"
-when 425   #ワーウルフ
-   return "Werewolf"
-when 426   #ワーウルフ
-   return "Werewolf"
-when 427   #ワーキャット
-   return "Werecat"
-when 428   #ワーキャット
-   return "Werecat"
-when 429   #ゴブリン
-   return "Goblin"
-when 430   #ギャングコマンダー
-   return "Gang Leader"
-when 431   #プリーステス
-   return "Priestess"
-when 432   #カースメイガス
-   return "Cursed Magus"
-when 433   #アルラウネ
-   return "Alraune"
-when 434   #アルラウネ
-   return "Alraune"
-when 435   #マタンゴ
-   return "Matango"
-when 436   #ダークエンジェル
-   return "Dark Angel"
-when 437   #ガーゴイル
-   return "Gargoyle"
-when 438   #ミミック
-   return "Mimic"
-when 439   #ミミック
-   return "Mimic"
-when 440   #タマモ
-   return "Tamano"
-when 441   #リリム
-   return "Lilim"
-when 442   #ネイジュレンジ
-   return "Neijoronge"
-when 443   #リジェオ
-   return "Rejeo"
-when 444   #フルビュア
-   return "Fulbeua"
-when 445   #ギルゴーン
-   return "Gilgoon"
-when 446   #ユーガノット
-   return "Younganoth"
-when 447   #シルフェ
-   return "Syplhe"
-when 448   #ラーミル
-   return "Rarmil"
-when 449   #ヴェルミィーナ
-   return "Vermiena"
-when 450   #キャスト
-   return "Caster"
+  return "Rarmil Cast"
    
-   #[DATA] enemies
-when 1   #データベース
-   return "データベース"
-when 2   #尻尾持ち
-   return "尻尾持ち"
-when 3   #チュートリアル
-   return "チュートリアル"
-when 4   #ボス用基本
-   return "ボス用基本"
-when 5   #データベース（消極）
-   return "データベース（消極）"
-when 501   #レッサーサキュバス橙
-   return "レッサーサキュバス橙"
-when 502   #レッサーサキュバス桃
-   return "レッサーサキュバス桃"
-when 503   #サキュバス橙
-   return "サキュバス橙"
-when 504   #サキュバス桃
-   return "サキュバス桃"
-when 505   #サキュバスロード橙
-   return "サキュバスロード橙"
-when 506   #サキュバスロード橙
-   return "サキュバスロード橙"
-when 508   #インプ緑
-   return "インプ緑"
-when 509   #インプ白
-   return "インプ白"
-when 510   #デビル緑
-   return "デビル緑"
-when 511   #デビル白
-   return "デビル白"
-when 512   #デーモン緑
-   return "デーモン緑"
-when 513   #デーモン白
-   return "デーモン白"
-when 515   #プチウィッチ緑
-   return "プチウィッチ緑"
-when 516   #プチウィッチ紫
-   return "プチウィッチ紫"
-when 517   #ウィッチ緑
-   return "ウィッチ緑"
-when 518   #ウィッチ紫
-   return "ウィッチ紫"
-when 520   #キャスト黄
-   return "キャスト黄"
-when 522   #スレイヴ紫
-   return "スレイヴ紫"
-when 524   #ナイトメア黒
-   return "ナイトメア黒"
-when 525   #ナイトメア黄
-   return "ナイトメア黄"
-when 527   #スライム青
-   return "スライム青"
-when 530   #ゴールドスライム黄
-   return "ゴールドスライム黄"
-when 532   #ファミリア青
-   return "ファミリア青"
-when 533   #ファミリア緑
-   return "ファミリア緑"
-when 535   #ワーウルフ黒
-   return "ワーウルフ黒"
-when 536   #ワーウルフ赤
-   return "ワーウルフ赤"
-when 538   #ワーキャット黄
-   return "ワーキャット黄"
-when 539   #ワーキャット黒
-   return "ワーキャット黒"
-when 541   #ゴブリン赤
-   return "ゴブリン赤"
-when 543   #ギャングコマンダー赤
-   return "ギャングコマンダー赤"
-when 546   #プリーステス白
-   return "プリーステス白"
-when 548   #カースメイガス黒
-   return "カースメイガス黒"
-when 550   #アルラウネ緑
-   return "アルラウネ緑"
-when 551   #アルラウネ青
-   return "アルラウネ青"
-when 553   #マタンゴ赤
-   return "マタンゴ赤"
-when 555   #ダークエンジェル白
-   return "ダークエンジェル白"
-when 557   #ガーゴイル黒
-   return "ガーゴイル黒"
-when 559   #ミミック青
-   return "ミミック青"
-when 560   #ミミック黒
-   return "ミミック黒"
-when 562   #タマモ赤
-   return "タマモ赤"
-when 564   #リリム桃
-   return "リリム桃"
-when 566   #ネイジュレンジ
-   return "Neijoronge"
-when 567   #リジェオ
-   return "Rejeo"
-when 568   #フルビュア
-   return "Fulbeua"
-when 569   #ギルゴーン
-   return "Gilgoon"
-when 570   #ユーガノット
-   return "Younganoth"
-when 571   #シルフェ
-   return "Syplhe"
-when 572   #ラーミル
-   return "Rarmil"
-when 573   #ヴェルミィーナ
-   return "Vermiena"
-when 575   #キャスト黒
-   return "キャスト黒"
-      
-   # [fix] enemies
-when 26   #ワーウルフ
-   return "Werewolf"
-   
-   
+  #OFEs
+when 501   #【data】レッサーサキュバス橙
+  return "Orange Lesser Succubus"
+when 502   #【data】レッサーサキュバス桃
+  return "Peach Lesser Succubus"
+when 503   #【data】サキュバス橙
+  return "Orange Succubus"
+when 504   #【data】サキュバス桃
+  return "Peach Succubus"
+when 505,506   #【data】サキュバスロード橙
+  return "Orange Succubus Lord"
+when 508   #【data】インプ緑
+  return "Green Imp"
+when 509   #【data】インプ白
+  return "White Imp"
+when 510   #【data】デビル緑
+  return "Green Devil"
+when 511   #【data】デビル白
+  return "White Devil"
+when 512   #【data】デーモン緑
+  return "Green Demon"
+when 513   #【data】デーモン白
+  return "White Demon"
+when 515   #【data】プチウィッチ緑
+  return "Green Little Witch"
+when 516   #【data】プチウィッチ紫
+  return "Purple Little Witch"
+when 517   #【data】ウィッチ緑
+  return "Green Witch"
+when 518   #【data】ウィッチ紫
+  return "Purple Witch"
+when 520   #【data】キャスト黄
+  return "Yellow Caster"
+when 522   #【data】スレイヴ紫
+  return "Purple Slave"
+when 524   #【data】ナイトメア黒
+  return "Black Nightmare"
+when 525   #【data】ナイトメア黄
+  return "Yellow Nightmare"
+when 527   #【data】スライム青
+  return "Blue Slime"
+when 530   #【data】ゴールドスライム黄
+  return "Yellow Gold Slime"
+when 532   #【data】ファミリア青
+  return "Blue Familiar"
+when 533   #【data】ファミリア緑
+  return "Green Familiar"
+when 535   #【data】ワーウルフ黒
+  return "Black Werewolf"
+when 536   #【data】ワーウルフ赤
+  return "Red Werewolf"
+when 538   #【data】ワーキャット黄
+  return "Yellow Werecat"
+when 539   #【data】ワーキャット黒
+  return "Black Werecat"
+when 541   #【data】ゴブリン赤
+  return "Red Goblin"
+when 543   #【data】ギャングコマンダー赤
+  return "Red Gang Leader"
+when 546   #【data】プリーステス白
+  return "White Priestess"
+when 548   #【data】カースメイガス黒
+  return "Black Cursed Magus"
+when 550   #【data】アルラウネ緑
+  return "Green Alraune"
+when 551   #【data】アルラウネ青
+  return "Blue Alraune"
+when 553   #【data】マタンゴ赤
+  return "Red Matango"
+when 555   #【data】ダークエンジェル白
+  return "White Dark Angel"
+when 557   #【data】ガーゴイル黒
+  return "Black Gargoyle"
+when 559   #【data】ミミック青
+  return "Blue Mimic"
+when 560   #【data】ミミック黒
+  return "Black Mimic"
+when 562   #【data】タマモ赤
+  return "Red Tamano"
+when 564   #【data】リリム桃
+  return "Peach Lilim"
+when 575   #【data】キャスト黒
+  return "Black Caster"
+  
       end
-      text = @name.split(/\//)[0] rescue text = "error: no valid enemy name"
-#      text += " *TODO*" if text != nil
+      text = @name.split(/\//)[0] rescue text = "enemy #{@id}: name error"
+      text.sub!(/【(.*)】/,"")
       return text
     end
   end
 end
+
+
+=begin
+#***method safekeeping
+
+def build_inma
+  b = []
+  a = ""
+  sorted = []
+  namebase = []
+  
+  # sorting process for duplicates
+  for i in $data_enemies
+   if i != nil and i.name != nil and i.name != "" and i.name["■"] != "■"
+    oldid = i.id.to_s
+    oldname = i.name.split(/\//)[0]
+      oldname.sub!(/【(.*)】/,"")
+    namebase.push(oldname)
+    if namebase.uniq == namebase
+      sorted.push([oldid,oldname,i.name.split(/\//)[0]])
+    # sorted[x][0] = "id" (string)
+    # sorted[x][1] = "clean name"
+    # sorted[x][2] = "full original name w/o suffixes" 
+    else
+      namebase.uniq!
+      for j in sorted
+        j[0] = j[0] + "," + oldid if j[1] == oldname
+      end
+    end
+   end
+  end
+
+  for i in sorted
+    a = "when " + i[0] + "   \#" + i[2]
+      b.push(a)
+    a = "  return \"" + i[1] + "\""
+#    a = "  return \"" + i[1].translation_check + "\""
+#    a = "  return \"" + splitprefix(i[1])[0] + splitprefix(i[1])[1] + "\""
+      b.push(a)
+  end
+
+    open("zzz.txt","a+") do |log|
+    log.puts b
+    end
+end
+  
+
+def splitprefix(name)
+    prefix = ""
+    prefix = "Orange " if name["橙"] == "橙"
+      name.gsub!("橙","")
+    prefix = "Peach " if name["桃"] == "桃"
+      name.gsub!("桃","")
+    prefix = "Green " if name["緑"] == "緑"
+      name.gsub!("緑","")
+    prefix = "White " if name["白"] == "白"
+      name.gsub!("白","")
+    prefix = "Purple " if name["紫"] == "紫"
+      name.gsub!("紫","")
+    prefix = "Black " if name["黒"] == "黒"
+      name.gsub!("黒","")
+    prefix = "Yellow " if name["黄"] == "黄"
+      name.gsub!("黄","")
+    prefix = "Red " if name["赤"] == "赤"
+      name.gsub!("赤","")
+    prefix = "Blue " if name["青"] == "青"
+      name.gsub!("青","")
+    return [prefix,name]
+    #splitprefix("catblack") -> ["black ", "cat"]
+end
+  
+=end  
